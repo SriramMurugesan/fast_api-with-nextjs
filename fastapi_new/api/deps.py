d536c765e1bb1ca4bc865a5d5867151ca8e38241
SriramMurugesan <sri2014ram@gmail.com>
2025-04-18 21:13:07 +0530

    1 hr

--- fastapi_new/api/deps.py
+++ fastapi_new/api/deps.py
@@ -3,15 +3,15 @@ from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
-from jose import JWTError, jwt
-import os
+from jose import jwt, JWTError
 from dotenv import load_dotenv
+import os
 from .database import SessionLocal
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("AUTH_SECRET")
-ALGORITHM = os.getenv("AUTH_ALGORITHM")
+SECRET_KEY = os.getenv('AUTH_SECRET_KEY')
+ALGORITHM = os.getenv('AUTH_ALGORITHM')
 
 def get_db():
     db = SessionLocal()
@@ -19,27 +19,25 @@ def get_db():
         yield db
     finally:
         db.close()
+        
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
-oauth2_bearer_dependency = Annotated[str, Depends(oauth2_scheme)]
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
+oauth2_bearer_dependency = Annotated[str, Depends(oauth2_bearer)]
+
 
 async def get_current_user(token: oauth2_bearer_dependency):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        user_id: int = payload.get("id")
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
         if username is None or user_id is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid authentication credentials",
-            )
-        return {"username": username, "user_id": user_id}
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+        return {'username': username, 'id': user_id}
     except JWTError:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid authentication credentials",
-            )
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+    
+
 user_dependency = Annotated[dict, Depends(get_current_user)]
--- fastapi_new/api/routers/routines.py
+++ fastapi_new/api/routers/routines.py
@@ -1,29 +1,29 @@
 from pydantic import BaseModel
-from typing import Optional, List
-from fastapi import APIRouter, Depends, HTTPException, status
+from typing import List, Optional
+from fastapi import APIRouter
 from sqlalchemy.orm import joinedload
-from api.models import Routine as RoutineModel, Workout
+from api.models import Workout, Routine
 from api.deps import db_dependency, user_dependency
 
 router = APIRouter(
-    prefix="/routines",
-    tags=["Routines"]
+    prefix='/routines',
+    tags=['routines']
 )
 
 class RoutineBase(BaseModel):
     name: str
     description: Optional[str] = None
-
+    
 class RoutineCreate(RoutineBase):
     workouts: List[int] = []
-
+    
 @router.get("/")
 def get_routines(db: db_dependency, user: user_dependency):
-    return db.query(RoutineModel).options(joinedload(RoutineModel.workouts)).filter(RoutineModel.user_id == user.get('user_id')).all()
+    return db.query(Routine).options(joinedload(Routine.workouts)).filter(Routine.user_id == user.get('id')).all()
 
 @router.post("/")
 def create_routine(db: db_dependency, user: user_dependency, routine: RoutineCreate):
-    db_routine = RoutineModel(name=routine.name, description=routine.description, user_id=user.get('user_id'))
+    db_routine = Routine(name=routine.name, description=routine.description, user_id=user.get('id'))
     for workout_id in routine.workouts:
         workout = db.query(Workout).filter(Workout.id == workout_id).first()
         if workout:
@@ -31,12 +31,12 @@ def create_routine(db: db_dependency, user: user_dependency, routine: RoutineCre
     db.add(db_routine)
     db.commit()
     db.refresh(db_routine)
-    db_routines = db.query(RoutineModel).options(joinedload(RoutineModel.workouts)).filter(RoutineModel.id == db_routine.id).first()
+    db_routines = db.query(Routine).options(joinedload(Routine.workouts)).filter(Routine.id == db_routine.id).first()
     return db_routines
 
-@router.delete("/")
+@router.delete('/')
 def delete_routine(db: db_dependency, user: user_dependency, routine_id: int):
-    db_routine = db.query(RoutineModel).filter(RoutineModel.id == routine_id).first()
+    db_routine = db.query(Routine).filter(Routine.id == routine_id).first()
     if db_routine:
         db.delete(db_routine)
         db.commit()
